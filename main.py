db0cf84ec747ad9d40ca34d110ec20f3c2e2a496
Artem-Shabaltun <134101240+Artem-Shabaltun@users.noreply.github.com>
2023-09-03 18:26:51 +0300

    last update
    test 1

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ class ADDRESSBOOK(UserDict):
             if isinstance(record.name.value, str) and query.lower() in record.name.value.lower():
                 results.append(record)
             for phone in record.phones:
-                if query in phone.value:
+                if query == phone.value:  # Зміна тут: порівнюємо замість перевірки входження
                     results.append(record)
                     break
         return results
-
 
 # Зберігання адресної книги до файлу
 def save_address_book(address_book, filename):
@@ -68,34 +67,32 @@ class Email(Field):
 
 
 class Record:
-    def __init__(self, name, birthday=None):
+    def __init__(self, name, phone, email, birthday=None):
         self.name = Name(name)
-        self.phones = []
-        self.emails = []
+        self.phones = [Phone(phone)]
+        self.emails = [Email(email)]
         self.birthday = birthday
 
-    def add_phone(self, number: Phone):
-        phone_number = Phone(number)
-        if phone_number not in self.phones:
-            self.phones.append(phone_number)
+    def add_phone(self, number):
+        if number not in [phone.value for phone in self.phones]:
+            self.phones.append(Phone(number))
 
     def update_phone(self, old_number, new_number):
-        old_phone = Phone(old_number)
         for i in range(len(self.phones)):
-            if self.phones[i] == old_phone:
+            if self.phones[i].value == old_number:
                 self.phones[i] = Phone(new_number)
 
     def delete_phone(self, value):
         self.phones = [phone for phone in self.phones if phone.value != value]
 
-    def add_email(self, email: Email):
-        email_address = Email(email)
-        if email_address not in self.emails:
-            self.emails.append(email_address)
+    def add_email(self, email):
+        if email not in [e.value for e in self.emails]:
+            self.emails.append(Email(email))
 
     def update_email(self, old_email, new_email):
-        index = self.emails.index(old_email)
-        self.emails[index] = new_email
+        for i in range(len(self.emails)):
+            if self.emails[i].value == old_email:
+                self.emails[i] = Email(new_email)
 
     def delete_email(self, value):
         self.emails = [email for email in self.emails if email.value != value]
@@ -130,83 +127,42 @@ class Birthday(Field):
         if not self.is_valid_birthday(value):
             raise ValueError("Incorrect Birthday")
 
-
 if __name__ == "__main__":
-    try:
-        restored_address_book = load_address_book('address_book.pkl')
-    except FileNotFoundError:
-        restored_address_book = ADDRESSBOOK()
+    # Створення декількох контактів
+    name1 = Name("Art")
+    phone1 = Phone("023456789")
+    email1 = Email("art@gmail.com")
+    birthday1 = Birthday(datetime(1997, 7, 10))
 
-    name = Name('Nick')
-    phone = Phone('987654321')
-    rec = Record(name, phone)
-    ab = ADDRESSBOOK()
-    ab.add_record(rec)
-    ab['Nick'] = rec
-    ab['Nick'].add_phone('987654321')
+    name2 = Name("Nick")
+    phone2 = Phone("987654321")
+    email2 = Email("Nick@gmail.com")
+    birthday2 = Birthday(datetime(1997, 8, 9))
 
-    assert isinstance(ab['Nick'], Record)
-    assert isinstance(ab['Nick'].name, Name)
-    assert isinstance(ab['Nick'].phones, list)
-    assert len(ab['Nick'].phones) > 0
-    assert isinstance(ab['Nick'].phones[0], Phone)
-    assert ab['Nick'].phones[0].value == '987654321'
-    print('Test 1 : OK')
+    # Створення записів в книгу
+    record1 = Record(name1, phone1, email1, birthday1)
+    record2 = Record(name2, phone2, email2, birthday2)
 
-    name = Name('Art')
-    phone = Phone('023456789')
-    rec = Record(name, phone)
-    ab = ADDRESSBOOK()
-    ab.add_record(rec)
-    ab['Art'] = rec
-    ab['Art'].add_phone('023456789')
+    # Додавання та редагування контактів
+    record1.add_phone("11111111")
+    record2.update_email("Nick@gmail.com", "Nickdiss@gmail.com")
 
-    assert isinstance(ab['Art'], Record)
-    assert isinstance(ab['Art'].name, Name)
-    assert isinstance(ab['Art'].phones, list)
-    assert len(ab['Art'].phones) > 0
-    assert isinstance(ab['Art'].phones[0], Phone)
-    assert ab['Art'].phones[0].value == '023456789'
-    print('Test 2 : OK')
+    # Створення адресної книги та додавання записів
+    address_book = ADDRESSBOOK()
+    address_book.add_record(record1)
+    address_book.add_record(record2)
 
-    name = Name('Alice')
-    phone = Phone('555555555')
-    rec = Record(name, phone)
-    ab = ADDRESSBOOK()
-    ab.add_record(rec)
-    ab['Alice'] = rec
-    ab['Alice'].add_phone('555555555')
+    # Збереження адресної книги до файлу
+    save_address_book(address_book, "address_book.pkl")
 
-    assert isinstance(ab['Alice'], Record)
-    assert isinstance(ab['Alice'].name, Name)
-    assert isinstance(ab['Alice'].phones, list)
-    assert len(ab['Alice'].phones) > 0
-    assert isinstance(ab['Alice'].phones[0], Phone)
-    assert ab['Alice'].phones[0].value == '555555555'
-    print('Test 3 : OK')
-    
-    save_address_book(ab, 'address_book.pkl')
+    # Завантаження адресної книги
+    loaded_address_book = load_address_book("address_book.pkl")
 
-    print("Записи в адресній книзі:")
-    for record in restored_address_book:
-        print(f"Ім'я: {record.name.value}")
-        print(f"Телефони: {', '.join(phone.value for phone in record.phones)}")
-        print(f"Електронні адреси: {', '.join(email.value for email in record.emails)}")
-        print(f"День народження: {record.birthday.value if record.birthday else 'Немає'}")
-        print()
-
-    # search_query = input("Введіть ім'я або номер телефону для пошуку: ")
-    # search_results = ab.search_by_name_or_phone(search_query)
-
-    # if search_results:
-    #     print("Результати пошуку:")
-    #     for result in search_results:
-    #         print(f"Ім'я: {result.name.value}")
-    #         print(f"Телефони: {', '.join(phone.value for phone in result.phones)}")
-    #         print(f"Електронні адреси: {', '.join(email.value for email in result.emails)}")
-    #         print(f"Дні до дня народження: {result.calculate_days_to_birthday()}")
-    #         print()
-    # else:
-    #     print("Нічого не знайдено.")
-
-
+    search_str = "11111111"
+    search_result = loaded_address_book.search_by_name_or_phone(search_str)
+    if search_result:
+        print(f"Search results for '{search_str}':")
+        for result in search_result:
+            print(result.name.value)
+    else:
+        print(f"No results found for '{search_str}'")
